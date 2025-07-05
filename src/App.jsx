52edc247d13c435b316fe55920a7f25fcb567553
Gaurav Uniyal <gauravuniyal@Gauravs-MacBook-Air.local>
2025-07-05 14:15:00 +0530

    Add App component to resolve build error

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, I'm Gaurav Uniyal 🚀</h1>
+      <p>This is a placeholder React app. Full portfolio coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
